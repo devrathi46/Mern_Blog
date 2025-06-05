ae5ff68c94f25fd0246872f05f1b70a0cf698ee9
devrathi46 <devrathi67155@gmal.com>
2025-06-05 15:41:50 +0530

    Create Signin api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,28 @@
-import { BrowserRouter, Routes , Route }  from "react-router-dom"
-import SignIn from "./pages/SignIn"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Projects from "./pages/Projects"
-import SignUp from "./pages/SignUp"
-import Dashboard from "./pages/Dashboard"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
-      <Header />
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/dashBoard" element={<Dashboard />} />
-      </Routes>
-      <Footer />
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/dashBoard' element={<Dashboard />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
-  )
+  );
 }
