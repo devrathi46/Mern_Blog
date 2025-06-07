93b733d11a10e4f005af3f60799c60fdd9feabbf
devrathi46 <devrathi67155@gmal.com>
2025-06-07 18:03:16 +0530

    add comment section to the admin dashboard

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -4,6 +4,7 @@ import {
   createComment,
   getPostComments,
   likeComment,
+  getcomments,
 } from '../controllers/comment.controller.js';
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.get('/getcomments', verifyToken, getcomments);
 
 export default router;
