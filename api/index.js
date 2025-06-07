bdb5be5252c5049a167f2c30786a73a5b248d652
devrathi46 <devrathi67155@gmal.com>
2025-06-07 18:14:57 +0530

    pre installations

--- api/index.js
+++ api/index.js
@@ -1,23 +1,25 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
 import dotenv from 'dotenv';
-dotenv.config();
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
-
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
     console.log('MongoDb is connected');
   })
-   .catch((err) => console.error('MongoDB connection error:', err));
-  
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -32,6 +34,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
